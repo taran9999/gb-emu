d856db83fde5e1982161f68a74f03e9f5d879a3e
Taran D <taran184@gmail.com>
2025-05-27 18:30:31 -0700

    add cli file picker

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::env;
-use std::fs::File;
-use std::io::Read;
+use std::fs::{self, File};
+use std::io::{self, Read, Write};
 
 use bus::Bus;
 use cart::Cart;
@@ -11,14 +10,45 @@ mod cart;
 mod cpu;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let fpath = &args[1];
-    println!("fpath: {fpath}");
+    // read a directory for file selection
+    // currently hardcoded to a "roms" directory where the the project is being run from
+    let roms: Vec<_> = fs::read_dir("roms")
+        .expect("Error reading roms directory")
+        .filter_map(|f| f.ok())
+        .filter(|f| {
+            let path = f.path();
+            if !path.is_file() {
+                return false;
+            }
+            match path.extension().and_then(|ext| ext.to_str()) {
+                Some("gb") | Some("gbc") => true,
+                _ => false,
+            }
+        })
+        .collect();
 
-    // let mut test_header_rom: [u8; 336] = [0; 336];
-    // let ch = cart::CartHeader::read_rom(&test_header_rom);
+    println!("Found roms:");
+    for (i, f) in roms.iter().enumerate() {
+        if let Some(fname) = f.path().file_name().and_then(|os_str| os_str.to_str()) {
+            println!("{}. {}", i + 1, fname);
+        }
+    }
 
-    let mut file = File::open(&format!("roms/{fpath}")).expect("Error opening file.");
+    print!("Select rom by number: ");
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let fpath = match input.trim().parse::<usize>() {
+        Ok(n) if n >= 1 && n <= roms.len() => Some(roms[n - 1].path()),
+        _ => {
+            println!("Invalid selection.");
+            None
+        }
+    };
+
+    let mut file =
+        File::open(fpath.expect("Could not retrieve file path.")).expect("Error opening file.");
     let mut file_buffer = Vec::new();
     file.read_to_end(&mut file_buffer)
         .expect("Error reading file.");

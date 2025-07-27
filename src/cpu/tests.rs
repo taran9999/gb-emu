0786477fe3d3be54555838e522d7f928132106e4
Taran D <taran184@gmail.com>
2025-07-27 14:48:17 -0700

    move test add a r8 to its own function

--- src/cpu/tests.rs
+++ src/cpu/tests.rs
@@ -2,6 +2,10 @@ use super::*;
 use crate::bus::Bus;
 use crate::cart::Cart;
 
+// TODO: this macro doesn't work because the cart and bus can't live long enough if created inside
+// (i assumed these were C style macros until i tried this)
+// see if there is a way to avoid copy pasting the cpu setup in test cases
+
 // macro_rules! setup {
 //     ($($addr:expr => $value:expr),*) => {{
 //         let mut rom = [0u8; 32 * 1024];
@@ -17,17 +21,37 @@ use crate::cart::Cart;
 //     }};
 // }
 
-#[test]
-fn test_add_a_r8() {
+fn add_a_r8(
+    target_reg: Reg8Symbol,
+    a_init: u8,
+    target_init: u8,
+    a_expected: u8,
+    z_expected: bool,
+    n_expected: bool,
+    h_expected: bool,
+    c_expected: bool,
+) {
+    let opcode = match target_reg {
+        Reg8Symbol::B => 0x80,
+        Reg8Symbol::C => 0x81,
+        Reg8Symbol::D => 0x82,
+        Reg8Symbol::E => 0x83,
+        Reg8Symbol::H => 0x84,
+        Reg8Symbol::L => 0x85,
+        Reg8Symbol::A => 0x87,
+        Reg8Symbol::F => panic!("No opcode for ADD A F"),
+    };
+
     let mut rom = [0u8; 32 * 1024];
-    rom[0x100] = 0x80;
+    rom[0x100] = opcode;
 
     let mut cart = Cart::read_rom(&rom);
     let mut bus = Bus::new(&mut cart);
     let mut cpu = CPU::init(&mut bus);
 
-    cpu.a.0 = 1;
-    cpu.b.0 = 1;
+    cpu.a.0 = a_init;
+    let target = cpu.reg8_from_symbol(&target_reg);
+    target.0 = target_init;
 
     cpu.step();
 
@@ -37,33 +61,47 @@ fn test_add_a_r8() {
     let c = cpu.get_flag_c();
 
     assert!(
-        cpu.a.0 == 2,
+        cpu.a.0 == a_expected,
         "Incorrect value in register a: expected {}, got {}.",
-        2,
+        a_expected,
         cpu.a.0
     );
 
     assert!(
-        !z,
+        z == z_expected,
         "Incorrect value for flag z: expected {}, got {}",
-        false, z
+        z_expected,
+        z
     );
 
     assert!(
-        !n,
+        n == n_expected,
         "Incorrect value for flag z: expected {}, got {}",
-        false, n
+        n_expected,
+        n
     );
 
     assert!(
-        !h,
+        h == h_expected,
         "Incorrect value for flag z: expected {}, got {}",
-        false, h
+        h_expected,
+        h
     );
 
     assert!(
-        !c,
+        c == c_expected,
         "Incorrect value for flag z: expected {}, got {}",
-        false, c
+        c_expected,
+        c
     );
+}
+
+#[test]
+fn test_add_a_r8_no_carry() {
+    add_a_r8(Reg8Symbol::B, 1, 1, 2, false, false, false, false);
+}
+
+#[test]
+fn test_add_a_r8_no_carry_zero() {
+    add_a_r8(Reg8Symbol::B, 0, 0, 0, true, false, false, false);
 }

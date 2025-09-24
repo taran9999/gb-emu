41e8300bbf2053ead708ad1320a6b05408f0e7a1
Taran D <taran184@gmail.com>
2025-09-24 18:12:24 -0400

    add 0xFF0F interrupt flag to io

--- src/gbio.rs
+++ src/gbio.rs
@@ -5,6 +5,7 @@ pub struct Io<'a> {
     serial_data: u8,
     serial_control: u8,
     timer: &'a mut Timer,
+    int_flag: u8,
 }
 
 impl Io<'_> {
@@ -13,6 +14,7 @@ impl Io<'_> {
             serial_data: 0,
             serial_control: 0,
             timer,
+            int_flag: 0,
         }
     }
 
@@ -21,6 +23,7 @@ impl Io<'_> {
             0xFF01 => self.serial_data,
             0xFF02 => self.serial_control,
             0xFF04..=0xFF07 => self.timer.timer_read(address),
+            0xFF0F => self.int_flag,
             _ => panic!("io read out of bounds at ${:04X}", address),
         }
     }
@@ -30,6 +33,7 @@ impl Io<'_> {
             0xFF01 => self.serial_data = value,
             0xFF02 => self.serial_control = value,
             0xFF04..=0xFF07 => self.timer.timer_write(address, value),
+            0xFF0f => self.int_flag = value,
             _ => panic!("io write out of bounds at ${:04X}", address),
         }
     }
